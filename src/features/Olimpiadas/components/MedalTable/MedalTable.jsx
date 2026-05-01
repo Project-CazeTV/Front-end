a1404c09c5c8e12fe69d736071eb9d4e2e78e3ae
Gabriel <minifinikogama@gmail.com>
2026-05-01 05:31:37 -0300

    feat: página das olímpiadas de verão finalizada

--- src/features/Olimpiadas/components/MedalTable/MedalTable.jsx
+++ src/features/Olimpiadas/components/MedalTable/MedalTable.jsx
@@ -3,35 +3,37 @@ import { medalData } from '../../../../mocks/medalData';
 
 export default function MedalTable() {
   return (
-    <div className={styles.tableContainer}>
+    <section className={styles.sectionContainer}>
       <h3 className={styles.title}>Quadro de Medalhas</h3>
-      <table className={styles.table}>
-        <thead>
-          <tr>
+      <div className={styles.responsiveWrapper}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
               <th className={styles.rankHeader}>Ordem</th>
               <th className={styles.countryHeader}>País</th>
-              <th><img src="../../../../assets/Gold_medal.svg.png" alt="Ouro" className={styles.medalIcon} /></th>
-              <th><img src="../../../../assets/Silver_medal.svg.png"alt="Prata" className={styles.medalIcon} /></th>
-              <th><img src="../../../../assets/Bronze_medal.svg.png" alt="Bronze" className={styles.medalIcon} /></th>
-              <th><img src="../../../../assets/GoldSilverBronze_medals.svg.png" alt="Total" className={styles.medalIcon} /></th>
+              <th className={styles.medalHeader}><img src="/src/assets/Gold_medal.svg.png" alt="Ouro" className={styles.medalIcon} /></th>
+              <th className={styles.medalHeader}><img src="/src/assets/Silver_medal.svg.png" alt="Prata" className={styles.medalIcon} /></th>
+              <th className={styles.medalHeader}><img src="/src/assets/Bronze_medal.svg.webp" alt="Bronze" className={styles.medalIcon} /></th>
+              <th className={styles.totalHeader}><img src="/src/assets/GoldSilverBronze_medals.svg.png" alt="Total" className={styles.medalIcon} /></th>
             </tr>
-        </thead>
-        <tbody>
-          {medalData.map((item, index) => (
-            <tr key={item.id}>
-              <td>{index + 1}</td>
-              <td className={styles.countryCell}>
-                <span className={styles.flag}>{item.flag}</span>
-                {item.country}
-              </td>
-              <td>{item.gold}</td>
-              <td>{item.silver}</td>
-              <td>{item.bronze}</td>
-              <td className={styles.total}>{item.total}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {medalData.map((item, index) => (
+              <tr key={item.id} className={styles.row}>
+                <td className={styles.rankCell}>{index + 1}</td>
+                <td className={styles.countryCell}>
+                  <img className={styles.flag} src={item.flag}/>
+                  <span className={styles.countryName}>{item.country}</span>
+                </td>
+                <td className={`${styles.medalCell} ${styles.gold}`}>{item.gold}</td>
+                <td className={`${styles.medalCell} ${styles.silver}`}>{item.silver}</td>
+                <td className={`${styles.medalCell} ${styles.bronze}`}>{item.bronze}</td>
+                <td className={styles.totalCell}>{item.total}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
   );
 }
--- src/pages/OlympicsPage/OlympicsPage.jsx
+++ src/pages/OlympicsPage/OlympicsPage.jsx
@@ -1,19 +1,111 @@
-import MainHeader from '../../components/Layout/MainHeader/MainHeader';
-import CommonFooter from '../../components/Layout/CommonFooter/CommonFooter';
-import CountdownCard from '../../features/WorldCup/components/CountdownCard/CountdownCard';
-import FilterSection from '../../components/Common/FilterSection/FilterSection';
-import ColoredHeader from '../../components/Layout/ColoredHeader/ColoredHeader';
-import MedalTable from '../../features/Olimpiadas/components/MedalTable/MedalTable';
-import styles from './OlympicsPage.module.css'
+import MainHeader from "../../components/Layout/MainHeader/MainHeader";
+import CommonFooter from "../../components/Layout/CommonFooter/CommonFooter";
+import CountdownCard from "../../features/WorldCup/components/CountdownCard/CountdownCard";
+import FilterSection from "../../components/Common/FilterSection/FilterSection";
+import ColoredHeader from "../../components/Layout/ColoredHeader/ColoredHeader";
+import MedalTable from "../../features/Olimpiadas/components/MedalTable/MedalTable";
+import NewsList from "../../components/Common/NewsList/NewsList";
+import styles from "./OlympicsPage.module.css";
+import { useState } from "react";
+export default function OlympicsPage() {
+  const noticiasMenoresMock = [
+    {
+      id: 1,
+      categoria: "Apresentação de Abertura",
+      tagColor: "#1E40AF",
+      titulo: "Lady Gaga canta na abertura das Olimpíadas Paris 2024 | Gshow",
+      tempo: "12 DIAS",
+      imagem: "src/assets/gaga.png",
+    },
+    {
+      id: 2,
+      categoria: "Salto Artístico Feminino",
+      tagColor: "#DC2626",
+      titulo:
+        "É Brasil!!! Rebeca Andrade conquista ouro em Salto Artístico e supera Simone Biles.",
+      tempo: "1 DIA",
+      imagem: "src/assets/salto_fem.png",
+    },
+    {
+      id: 3,
+      categoria: "Paris 2024",
+      tagColor: "#22C55E",
+      titulo:
+        "Rio Sena, após anos, volta a se tornar banhável na França e agora é possível modalidades aquáticas.",
+      tempo: "20 DIAS",
+      imagem: "src/assets/identidade-visual-das-olimpiadas-de-paris-2024.png",
+    },
+  ];
 
-export default function OlympicsPage(){
-    return (
-           <div className={styles.pageContainer}>
-                <ColoredHeader></ColoredHeader>
-              <MainHeader isTransparent={true} />
-              <MedalTable />
-              </div>
-        
-        
-    )
+  const filtros = [
+    "Esportes Aquáticos",
+    "Atletismo",
+    "Ginástica",
+    "Esportes de Combate",
+    "Esportes de Equipe/Quadra",
+    "Ciclismo",
+  ];
+
+
+const sports = [
+  "Natação",
+  "Saltos Ornamentais",
+  "Maratona Aquática",
+  "Polo Aquático",
+  "Nado Artístico"
+];
+
+  const [activeSport, setActiveSport] = useState("Natação");
+
+  return (
+    <div className={styles.pageContainer}>
+      <ColoredHeader></ColoredHeader>
+      <MainHeader isTransparent={false} />
+      <section className={styles.liveSection}>
+        <div className={styles.liveHeader}>
+          <span>Assistir Ao Vivo</span>
+          <img
+            src="src\assets\pngtree-red-live-button-icon-png-image_17408319.png"
+            alt="ao vivo"
+          />
+        </div>
+
+        <div className={styles.liveBanner}>
+          <img src="src\assets\image11.png" alt="Cazé TV" />
+        </div>
+
+        <p className={styles.liveInfo}>
+          • Ao Vivo - Salto Artístico Feminino - Grupo A
+        </p>
+      </section>
+
+      <MedalTable />
+      <FilterSection titulo="Categorias Esportivas" filtros={filtros} />
+
+      <section className={styles.sportsSection}>
+        <div className={styles.sportsContainer}>
+          <div className={styles.sportsTop}>
+            <span className={styles.sportsTitle}>Esportes</span>
+            <span className={styles.sportsActive}>Esportes Aquáticos</span>
+            <span className={styles.sportsIcon}>🏊</span>
+          </div>
+
+          <ul className={styles.sportsItems}>
+  {sports.map((sport) => (
+    <li
+      key={sport}
+      className={activeSport === sport ? styles.activeItem : ""}
+      onClick={() => setActiveSport(sport)}
+    >
+      {sport}
+    </li>
+  ))}
+</ul>
+        </div>
+      </section>
+
+      <NewsList noticias={noticiasMenoresMock} />
+      <CommonFooter />
+    </div>
+  );
 }

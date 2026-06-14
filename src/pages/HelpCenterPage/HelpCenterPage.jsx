66dc4a84d7431f6ab6302e87bf0f32ad0e018b3b
Taylan <taylansilva0402@gmail.com>
2026-06-14 00:18:50 -0300

    fix: removendo mock destinado a central de ajuda de dentro dos componentes

--- src/pages/HelpCenterPage/HelpCenterPage.jsx
+++ src/pages/HelpCenterPage/HelpCenterPage.jsx
@@ -1,44 +1,10 @@
-
 import styles from "./HelpCenterPage.module.css";
-
 import ColoredHeader from "../../components/Layout/ColoredHeader/ColoredHeader";
 import MainHeader from "../../components/Layout/MainHeader/MainHeader";
 import CommonFooter from "../../components/Layout/CommonFooter/CommonFooter";
 import { Link } from "react-router-dom";
 import { FaWhatsapp } from "react-icons/fa";
-
-const helpTopics = [
-    {
-        title: "Conta e Acesso",
-        description: "Login, cadastro, recuperação de senha e gerenciamento da conta.",
-        route: "/ajuda/conta-acesso",
-    },
-    {
-        title: "Loja",
-        description: "Compras, carrinho, pedidos e produtos oficiais da CazéTV.",
-        route: "/ajuda/loja",
-    },
-    {
-        title: "Notícias",
-        description: "Como acompanhar notícias, matérias e conteúdos exclusivos.",
-        route: "/ajuda/noticias",
-    },
-    {
-        title: "Campeonatos",
-        description: "Informações sobre Brasileirão, Libertadores e outras competições.",
-        route: "/ajuda/campeonatos",
-    },
-    {
-        title: "Plataforma",
-        description: "Navegação, funcionalidades e utilização do site.",
-        route: "/ajuda/plataforma",
-    },
-    {
-        title: "Suporte Técnico",
-        description: "Problemas de carregamento, erros e dúvidas técnicas.",
-        route: "/ajuda/suporte-tecnico",
-    },
-];
+import { helpTopics } from "../../mocks/helpCenter";
 
 export default function HelpCenterPage() {
     return (

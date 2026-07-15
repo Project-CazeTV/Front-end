a6207d6d7ea3fe647e556b5b6c75c8d751092c21
Taylan <taylansilva0402@gmail.com>
2026-07-14 22:20:23 -0300

    fix: alterando mock

--- src/mocks/videos/videos.js
+++ src/mocks/videos/videos.js
@@ -1,17 +1,17 @@
 export const videosHome = [
     {
         id: "-OaghjvIqow",
-        title: "MELHORES MOMENTOS: FRANÇA 0 X 2 ESPANHA | COPA DO MUNDO FIFA™ 2026 | SEMIFINAL",
-        category: "Copa do Mundo"
+        title: "França 0 x 2 Espanha | Melhores Momentos",
+        category: "Semifinal • Copa do Mundo"
     },
     {
         id: "FOdbAWK1vO4",
-        title: "MELHORES MOMENTOS: ARGENTINA 3 X 1 SUÍÇA | COPA DO MUNDO FIFA™ 2026 | QUARTAS DE FINAL",
-        category: "Copa do Mundo"
+        title: "Argentina 3 x 1 Suíça | Melhores Momentos",
+        category: "Quartas de Final • Copa do Mundo"
     },
     {
         id: "KnwHI27RqXA",
-        title: "MELHORES MOMENTOS: BRASIL 1 X 2 NORUEGA | COPA DO MUNDO FIFA™ 2026 | OITAVAS DE FINAL",
-        category: "Copa do Mundo"
+        title: "Brasil 1 x 2 Noruega | Melhores Momentos",
+        category: "Oitavas de Final • Copa do Mundo"
     }
-]
+];
